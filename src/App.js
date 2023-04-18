015fc286b78c1c46dea2a67a2f06ca506f848250
Joseph Ogayo <mordestporcket27@gmail.com>
2023-04-18 19:26:54 +0300

    React Appcomponents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default App;
