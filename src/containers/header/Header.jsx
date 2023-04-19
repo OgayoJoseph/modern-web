753bdb6f2689d7727855bb93944c0f06e0468ceb
Joseph Ogayo <mordestporcket27@gmail.com>
2023-04-19 21:46:09 +0300

    Designing the Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -30,9 +30,9 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
-        <div className="gpt3__header-img">
+        {/* <div className="gpt3__header-img">
           <img src={ai} alt="AI" />
-        </div>
+        </div> */}
       </div>
     </header>
   );
