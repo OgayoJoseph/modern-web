8deae4ca3f4f60e33c3b333b67323eada238b830
Joseph Ogayo <mordestporcket27@gmail.com>
2023-04-19 23:30:28 +0300

    some modification and fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
         )}
 
         {toggleMenu && (
-          <div className="gpt3__navbar-menu_container navbar__scale-up-center">
+          <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
               <div className="gpt3__navbar-menu_container-links-sign">
