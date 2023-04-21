0fdc51018b8e0e608b152f90132e092a8e8a7bf7
Joseph Ogayo <mordestporcket27@gmail.com>
2023-04-21 15:25:15 +0300

    adding media queries and some display styles

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,12 +5,12 @@ import "./whatGPT3media.css";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section margin" id="wgpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
