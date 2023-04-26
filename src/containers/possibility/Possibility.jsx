1b45447695f17ca658ffe09eb70f0a6463e1fdf1
Joseph Ogayo <mordestporcket27@gmail.com>
2023-04-26 18:50:59 +0300

    implementing media queries

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import PossibilityImage from "../../assets/possibility.png";
 
 import "./possibility.css";
-import PossibilityImage from "../../assets/possibility.png";
 import "./possibilitymedia.css";
 
 const Possibility = () => {
   return (
-    <div className="gpt3__possibility sectio__padding" id="possibility">
+    <div className="gpt3__possibility section__padding" id="possibility">
       <div className="gpt3__possibility-image">
         <img src={PossibilityImage} alt="possibility" />
       </div>
